082e455d5f42bbc79b6ac75ee1883572d9c08cce
flamerten <flamerten@gmail.com>
2022-02-25 17:54:18 +0800

    Pulling data from carpark availability

--- usage_functions.py
+++ usage_functions.py
@@ -1,13 +1,28 @@
+from urllib import response
 import requests
 import datetime
 import pandas as pd
+import json
 
 #YYYY-MM-DD[T]HH:mm:ss (SGT)
 #trying to figure out how to work with the usage of the API
 
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
 today = datetime.datetime.today()
 params = {"data" : today.strftime("%Y-%m-%d") +"T" + today.strftime("%H:%M:%S")} # YYYY-MM-DD
 
-x = requests.get('https://api.data.gov.sg/v1/transport/carpark-availability',params = params).json()
+print(params)
 
-print(x.json.keys())
+response = requests.get('https://api.data.gov.sg/v1/transport/carpark-availability',params = params)
+data = response.json()
+
+if response.status_code == 200:
+    print("Data received successfully")
+else:
+    print(response.status_code)
+    print("Error")
