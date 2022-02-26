c930318942c01370b9246b3833ab352209001854
zst123 <zst123@users.noreply.github.com>
2022-02-27 07:29:29 +0800

    Add text files and redact token

---
+++ text_files.py
@@ -0,0 +1,19 @@
+def bot_token():
+    return 'insert your own token here'
+
+def start_text():
+    txt =  """ Welcome to carpark stats, a bot for you to check the capacity at nearby carparks
+    \nShare your location to receive the nearest carparks to you and their capacity
+    \nBlue Marker - Your current position
+    \nRed Marker - More than 75% full
+    \nYellow Marker - More Than 50% Full
+    \nGreen Marker Less than 50% Full"""
+
+    return txt
+
+def help_txt():
+    return """
+    These are all the various commands!
+
+Share your location to recieve the nearest carparks to you and their capacity
+    """
